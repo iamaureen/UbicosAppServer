32b9e327f255a38674ee444c28adc8c8c5ab88fd
Xiaoxin_He <xih85@pitt.edu>
2021-03-31 13:39:44 -0400

    minor fix in button hover, avoid keep creating span elements

--- textbook/app/static/js/buttonHover.js
+++ textbook/app/static/js/buttonHover.js
@@ -6,35 +6,39 @@ const hoverButtonMessage = function() {
             // console.log("mouse over")
             const activityButton = $(this);
             let type = activityButton.attr('class').replace('activity-button', '').trim();
-            // console.log(type)
 
-            if (type == 'video') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Watch Video </span>`);
+            if ($(this).find("span").length == 0) {
+                if (type == 'video') {
+                    console.log($(this).attr("class") + "has" + $(this).find("span").length);
+                    $('.activity-button.' + type).append(`<span class=${ className }> Watch Video </span>`);
+                }
+                if (type == 'whiteboard') {
+                    $('.activity-button.' + type).append(`<span class=${ className }> Go To White Board </span>`);
+                }
+                if (type == 'chatCard') {
+                    $('.activity-button.' + type).append(`<span class=${ className }> Chat With Others </span>`);
+                }
+                if (type == 'upload') {
+                    $('.activity-button.' + type).append(`<span class=${ className }> Upload Your Image </span>`);
+                }
+                if (type == 'individual') {
+                    $('.activity-button.' + type).append(`<span class=${ className }> Leave Comments On Images </span>`);
+                }
+                if (type == 'gallery') {
+                    $('.activity-button.' + type).append(`<span class=${ className }> Buggy Gallery </span>`);
+                }
+                if (type == 'self-gallery') {
+                    $('.activity-button.' + type).append(`<span class=${ className }> Review Comments </span>`);
+                }
+                if (type == 'khanacademy') {
+                    $('.activity-button.' + type).append(`<span class=${ className }> Badge Options </span>`);
+                }
+                if (type == 'table') {
+                    $('.activity-button.' + type).append(`<span class=${ className }> Buggy Lab Table </span>`);
+                }
             }
-            if (type == 'whiteboard') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Go To White Board </span>`);
-            }
-            if (type == 'chatCard') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Chat With Others </span>`);
-            }
-            if (type == 'upload') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Upload Your Image </span>`);
-            }
-            if (type == 'individual') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Leave Comments On Images </span>`);
-            }
-            if (type == 'gallery') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Buggy Gallery </span>`);
-            }
-            if (type == 'self-gallery') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Review Comments </span>`);
-            }
-            if (type == 'khanacademy') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Badge Options </span>`);
-            }
-            if (type == 'table') {
-                $('.activity-button.' + type).append(`<span class=${ className }> Buggy Lab Table </span>`);
-            }
+
+
 
         },
         // if mouse leave the button
@@ -42,8 +46,4 @@ const hoverButtonMessage = function() {
             // console.log("mouse leave");
         }
     );
-};
-
-module.exports = {
-    hoverButtonMessage: hoverButtonMessage
 };
