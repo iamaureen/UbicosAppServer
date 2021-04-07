57f558fb344734208e3d60879216a4ac22920526
Xiaoxin_He <xih85@pitt.edu>
2021-04-06 21:44:02 -0400

    clear

--- textbook/app/static/js/responsiveDesign.js
+++ textbook/app/static/js/responsiveDesign.js
@@ -1,11 +1,11 @@
-// add responsive design for the next and previous page button
-$(window).resize(function() {
-    var width = $('#textbook-content').width();
-    // console.log("current width", width);
+// // add responsive design for the next and previous page button
+// $(window).resize(function() {
+//     var width = $('#textbook-content').width();
+//     // console.log("current width", width);
 
-    $('#page-controls #page-control-previous').css("left", `${ width-100 }px`);
-    //add 45px */
+//     $('#page-controls #page-control-previous').css("left", `${ width-100 }px`);
+//     //add 45px */
 
-    $('#page-controls #page-control-next').css("left", `${ width-100+45 }px`);
+//     $('#page-controls #page-control-next').css("left", `${ width-100+45 }px`);
 
-});
+// });
