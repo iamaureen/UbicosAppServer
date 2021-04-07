dce5a1d88875720ed82aa17bb1ef14b2115aedf9
Xiaoxin_He <xih85@pitt.edu>
2021-04-06 20:14:41 -0400

    new

---
+++ textbook/app/static/js/responsiveDesign.js
@@ -0,0 +1,15 @@
+// // $(document).ready(function() {
+// //     alert("document ready occurred!");
+// // });
+
+// $(window).resize(function() {
+//     var width = $('#textbook-content').width();
+
+//     console.log("current width", width);
+
+
+
+//     if (width < 1000) {
+//         // alert(`${ width }Your screen is too small`);
+//     }
+// });
