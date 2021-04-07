620ee108a5a182fe35068fe3e2a79559a7cdd4ae
Xiaoxin_He <xih85@pitt.edu>
2021-04-06 20:53:01 -0400

    add responsive design dynamic using js

--- textbook/app/static/js/responsiveDesign.js
+++ textbook/app/static/js/responsiveDesign.js
@@ -1,15 +1,11 @@
-// // $(document).ready(function() {
-// //     alert("document ready occurred!");
-// // });
+// add responsive design for the next and previous page button
+$(window).resize(function() {
+    var width = $('#textbook-content').width();
+    // console.log("current width", width);
 
-// $(window).resize(function() {
-//     var width = $('#textbook-content').width();
+    $('#page-controls #page-control-previous').css("left", `${ width-100 }px`);
+    //add 45px */
 
-//     console.log("current width", width);
+    $('#page-controls #page-control-next').css("left", `${ width-100+45 }px`);
 
-
-
-//     if (width < 1000) {
-//         // alert(`${ width }Your screen is too small`);
-//     }
-// });
+});
