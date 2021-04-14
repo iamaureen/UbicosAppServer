6d4c0863ca0b2c0e294e08201a93397b31f0a0e1
Xiaoxin_He <xih85@pitt.edu>
2021-04-14 18:59:19 -0400

    new

--- textbook/app/static/js/kaform.js
+++ textbook/app/static/js/kaform.js
@@ -1,8 +1,8 @@
 var global_ka_url;
 var ka_act_id;
-var host_url=window.location.host;
+var host_url = window.location.host;
 
-$( function () {
+$(function() {
 
     ka_img_upload_display();
 
@@ -10,36 +10,36 @@ $( function () {
 
     ka_response_save();
 
-} );
+});
 
-var load_ka_card=function ( act_id, video_url ) {
+var load_ka_card = function(act_id, video_url) {
 
-    global_ka_url=video_url;
-    ka_act_id=act_id;
+    global_ka_url = video_url;
+    ka_act_id = act_id;
 
     //this method is defined in utility.js
-    computationalModelMethod( logged_in, 'KA', act_id );
+    computationalModelMethod(logged_in, 'KA', act_id);
 
-    enterLogIntoDatabase( 'Khan Academy Card Click', 'Khan Academy Card Load', '', global_current_pagenumber );
+    enterLogIntoDatabase('Khan Academy Card Click', 'Khan Academy Card Load', '', global_current_pagenumber);
 
 }
 
-$( "#ka-upload-img-form" ).hide();
+$("#ka-upload-img-form").hide();
 
 // display img upload option if a student selects 'answer' from the radio button
-var ka_img_upload_display=function () {
+var ka_img_upload_display = function() {
     //capture the radio button response
-    $( function () {
-        $( 'input:radio[name="ka-response-type"]' ).change( function () {
-            if ( $( this ).val()=="answer" ) {
+    $(function() {
+        $('input:radio[name="ka-response-type"]').change(function() {
+            if ($(this).val() == "answer") {
                 //if it is 'answer' display the image upload
-                $( "#ka-upload-img-form" ).show();
+                $("#ka-upload-img-form").show();
             } else {
                 //if it is 'question' do NOT display the image upload
-                $( "#ka-upload-img-form" ).hide();
+                $("#ka-upload-img-form").hide();
             }
-        } );
-    } );
+        });
+    });
 
 
 }
@@ -47,69 +47,74 @@ var ka_img_upload_display=function () {
 //capture whether a student uploaded an image, and complete
 //this will be similar to the method in upload.js
 //make an ajax call to uploadKAImage
-var ka_img_upload=function () {
+var ka_img_upload = function() {
 
     //get file information
 
 
-    $( "#ka_img_upload" ).change( function ( event ) {
-        console.log( "file changed" );
-        console.log( event )
-        var form_data=new FormData( $( '#ka-upload-img-form' )[ 0 ] );
-        console.log( form_data );
+    $("#ka_img_upload").change(function(event) {
+        console.log("file changed");
+        console.log(event)
+        var form_data = new FormData($('#ka-upload-img-form')[0]);
+        console.log(form_data);
 
-        $.ajax( {
+        $.ajax({
             type: 'POST',
-            url: 'http://'+host_url+'/uploadKAImage/',
+            url: 'http://' + host_url + '/uploadKAImage/',
             processData: false,
             contentType: false,
             async: false,
             cache: false,
             data: form_data,
-            success: function ( response ) {
-                console.log( response )
-                //success message
-                $( '.upload-success-msg' ).show();
+            success: function(response) {
+                console.log(response)
+                    //success message
+                $('.upload-success-msg').show();
 
-            }, error: function ( response ) {
-                console.log( "fail to upload" )
+            },
+            error: function(response) {
+                console.log("fail to upload")
             }
 
-        } );
+        });
 
-    } );
+    });
 
 
 
 }
 
 // capture students' response and save it in the database when pressed submit
-var ka_response_save=function () {
+var ka_response_save = function() {
 
     //detect when submit button is pressed, check for conditions, and then save
-    $( '#ka-submit' ).off().on( 'click', function ( event ) {
+    $('#ka-submit').off().on('click', function(event) {
 
         //get the response from the text area
-        var ka_response=$( '#KAAnswer' ).val();
-        console.log( "ka_response", ka_response );
+        var ka_response = $('#KAAnswer').val();
+        console.log("ka_response", ka_response);
 
-        var ka_response_type=$( 'input:radio[name="ka-response-type"]' ).val();
-        console.log( ka_response_type )
+        var ka_response_type = $('input:radio[name="ka-response-type"]').val();
+        console.log(ka_response_type)
 
         //save selected badge info to the database
-        $.ajax( {
+        $.ajax({
             type: 'POST',
             url: '/saveKApost',
             data: {
-                'username': logged_in, 'platform': 'KA', 'activity_id': ka_act_id, 'title': global_ka_url,
-                'response': ka_response, 'response_type': ka_response_type
+                'username': logged_in,
+                'platform': 'KA',
+                'activity_id': ka_act_id,
+                'title': global_ka_url,
+                'response': ka_response,
+                'response_type': ka_response_type
             },
-            success: function ( response ) {
-                console.log( response );
+            success: function(response) {
+                console.log(response);
             }
-        } );
+        });
 
-        enterLogIntoDatabase( 'KA Bagde Copy Button Click', 'Button Click', global_badge_selected, global_current_pagenumber );
+        enterLogIntoDatabase('KA response input', 'Button Click', global_badge_selected, global_current_pagenumber);
 
-    } );
+    });
 }
