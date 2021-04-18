a6ba21f5c81ac90558960636fd7c850b299d942f
Xiaoxin_He <xih85@pitt.edu>
2021-04-18 16:50:15 -0400

    merge conflicts

--- textbook/app/static/js/buttonHover.js
+++ textbook/app/static/js/buttonHover.js
@@ -1,42 +1,42 @@
-const hoverButtonMessage = function() {
-    const className = "hover-text";
-    $(".page a").hover(
+const hoverButtonMessage=function () {
+    const className="hover-text";
+    $( ".page a" ).hover(
         //if mouse is hovering the button
-        function() {
+        function () {
             // console.log("mouse over")
-            const activityButton = $(this);
+            const activityButton=$( this );
 
-            // console.log("button", activityButton);
-            let type = activityButton.attr('class').replace('activity-button', '').trim();
+            //console.log("button", activityButton);
+            let type=activityButton.attr( 'class' ).replace( 'activity-button', '' ).trim();
 
-            if ($(this).find("span").length == 0) {
-                if (type == 'video') {
-                    console.log($(this).attr("class") + "has" + $(this).find("span").length);
-                    $('.activity-button.' + type).append(`<span class=${ className }> Watch Video </span>`);
+            if ( $( this ).find( "span" ).length==0 ) {
+                if ( type=='video' ) {
+                    console.log( $( this ).attr( "class" )+"has"+$( this ).find( "span" ).length );
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Watch Video </span>` );
                 }
-                if (type == 'whiteboard') {
-                    $('.activity-button.' + type).append(`<span class=${ className }> Go To White Board </span>`);
+                if ( type=='whiteboard' ) {
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Go To White Board </span>` );
                 }
-                if (type == 'chatCard') {
-                    $('.activity-button.' + type).append(`<span class=${ className }> Chat With Others </span>`);
+                if ( type=='chatCard' ) {
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Chat With Others </span>` );
                 }
-                if (type == 'upload') {
-                    $('.activity-button.' + type).append(`<span class=${ className }> Upload Your Image </span>`);
+                if ( type=='upload' ) {
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Upload Your Image </span>` );
                 }
-                if (type == 'individual') {
-                    $('.activity-button.' + type).append(`<span class=${ className }> Leave Comments On Images </span>`);
+                if ( type=='individual' ) {
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Leave Comments On Images </span>` );
                 }
-                if (type == 'gallery') {
-                    $('.activity-button.' + type).append(`<span class=${ className }> Buggy Gallery </span>`);
+                if ( type=='gallery' ) {
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Buggy Gallery </span>` );
                 }
-                if (type == 'self-gallery') {
-                    $('.activity-button.' + type).append(`<span class=${ className }> Review Comments </span>`);
+                if ( type=='self-gallery' ) {
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Review Comments </span>` );
                 }
-                if (type == 'khanacademy') {
-                    $('.activity-button.' + type).append(`<span class=${ className }> Badge Options </span>`);
+                if ( type=='khanacademy' ) {
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Badge Options </span>` );
                 }
-                if (type == 'table') {
-                    $('.activity-button.' + type).append(`<span class=${ className }> Buggy Lab Table </span>`);
+                if ( type=='table' ) {
+                    $( '.activity-button.'+type ).append( `<span class=${ className }> Buggy Lab Table </span>` );
                 }
             }
 
@@ -44,7 +44,7 @@ const hoverButtonMessage = function() {
 
         },
         // if mouse leave the button
-        function() {
+        function () {
             // console.log("mouse leave");
         }
     );
--- textbook/app/static/js/kaform.js
+++ textbook/app/static/js/kaform.js
@@ -1,8 +1,9 @@
 var global_ka_url;
 var ka_act_id;
-var host_url = window.location.host;
+var host_url=window.location.host;
+var KA_imgID;
 
-$(function() {
+$( function () {
 
     ka_img_upload_display();
 
@@ -10,36 +11,40 @@ $(function() {
 
     ka_response_save();
 
-});
+} );
 
-var load_ka_card = function(act_id, video_url) {
+var load_ka_card=function ( act_id, video_url ) {
 
-    global_ka_url = video_url;
-    ka_act_id = act_id;
+    global_ka_url=video_url;
+    ka_act_id=act_id;
 
     //this method is defined in utility.js
-    computationalModelMethod(logged_in, 'KA', act_id);
+    //computationalModelMethod(logged_in, 'KA', act_id);
 
-    enterLogIntoDatabase('Khan Academy Card Click', 'Khan Academy Card Load', '', global_current_pagenumber);
+    enterLogIntoDatabase( 'Khan Academy Card Click', 'Khan Academy Card Load', '', global_current_pagenumber );
 
 }
 
-$("#ka-upload-img-form").hide();
+$( "#ka-upload-img-form" ).hide();
 
 // display img upload option if a student selects 'answer' from the radio button
-var ka_img_upload_display = function() {
+var ka_img_upload_display=function () {
     //capture the radio button response
-    $(function() {
-        $('input:radio[name="ka-response-type"]').change(function() {
-            if ($(this).val() == "answer") {
+    $( function () {
+        $( 'input:radio[name="ka-response-type"]' ).change( function () {
+            if ( $( this ).val()=="answer a question" ) {
                 //if it is 'answer' display the image upload
-                $("#ka-upload-img-form").show();
+                $( "#ka-upload-img-form" ).show();
+                $( "#default_ka_img_div" ).show();
             } else {
                 //if it is 'question' do NOT display the image upload
-                $("#ka-upload-img-form").hide();
+                $( "#ka-upload-img-form" ).hide();
+                $( "#default_ka_img_div" ).hide();
             }
-        });
-    });
+        } );
+    } );
+
+
 
 
 }
@@ -47,74 +52,136 @@ var ka_img_upload_display = function() {
 //capture whether a student uploaded an image, and complete
 //this will be similar to the method in upload.js
 //make an ajax call to uploadKAImage
-var ka_img_upload = function() {
+var ka_img_upload=function () {
 
     //get file information
 
 
-    $("#ka_img_upload").change(function(event) {
-        console.log("file changed");
-        console.log(event)
-        var form_data = new FormData($('#ka-upload-img-form')[0]);
-        console.log(form_data);
+    $( "#ka_img_upload" ).change( function ( event ) {
+        console.log( "file changed" );
+        console.log( event )
+        var form_data=new FormData( $( '#ka-upload-img-form' )[ 0 ] );
+        console.log( "form_data", form_data );
 
-        $.ajax({
+        $.ajax( {
             type: 'POST',
-            url: 'http://' + host_url + '/uploadKAImage/',
+            url: 'http://'+host_url+'/uploadKAImage/',
             processData: false,
             contentType: false,
             async: false,
             cache: false,
             data: form_data,
-            success: function(response) {
-                console.log(response)
-                    //success message
-                $('.upload-success-msg').show();
+            success: function ( response ) {
+                console.log( response );
+                //success message
+                $( '.upload-success-msg' ).show();
+                console.log( response.ka_imgID )
+                KA_imgID=response.ka_imgID;
 
-            },
-            error: function(response) {
-                console.log("fail to upload")
+            }, error: function ( response ) {
+                console.log( "khan academy image fail to upload" )
             }
 
-        });
+        } );
 
-    });
+    } );
 
 
+    // update preview image
+    $( "#ka_img_upload" ).change( function () {
+        //console.log( this )
+        ka_img_readURL( this );
+    } );
 
 }
+
+
+
+function ka_img_readURL( input ) {
+    console.log( input.files )
+
+    //console.log( input.files[ 0 ] )
+
+    if ( input.files&&input.files[ 0 ] ) {
+        var reader=new FileReader();
+        reader.onload=function ( e ) {
+            $( '#default_ka_img' ).attr( 'src', e.target.result );
+        }
+        reader.readAsDataURL( input.files[ 0 ] );
+    }
+}
+
+
 
 // capture students' response and save it in the database when pressed submit
-var ka_response_save = function() {
+var ka_response_save=function () {
 
     //detect when submit button is pressed, check for conditions, and then save
-    $('#ka-submit').off().on('click', function(event) {
+    $( '#ka-submit' ).off().on( 'click', function ( event ) {
+
+        //get the response type
+        var ka_response_type=$( 'input:radio[name="ka-response-type"]:checked' ).val();
+        console.log( ka_response_type )
 
         //get the response from the text area
-        var ka_response = $('#KAAnswer').val();
-        console.log("ka_response", ka_response);
+        var ka_response=$( '#KAAnswer' ).val();
+        console.log( "ka_response:: ", ka_response );
 
-        var ka_response_type = $('input:radio[name="ka-response-type"]').val();
-        console.log(ka_response_type)
+        var wasChecked=$( this ).data( 'checked' )
 
-        //save selected badge info to the database
-        $.ajax({
-            type: 'POST',
-            url: '/saveKApost',
+        if ( !$( '.ka-radio' ).is( ':checked' ) ) {
+            $( "#failure_text" ).text( "Please select radio !" )
+            $( '.upload-failure-msg' ).show();
+
+        } else if ( $( "#KAAnswer" ).val()===""||$( "#KAAnswer" ).val()===undefined ) {
+            $( "#failure_text" ).text( "Please enter text into response box!" )
+            $( '.upload-failure-msg' ).show();
+        }
+        else {
+
+            $( '.upload-failure-msg' ).hide();
+
+        }
+
+
+
+        $.post( {
+
+            url: '/submitKAAnswer',
+            async: false,
             data: {
-                'username': logged_in,
-                'platform': 'KA',
                 'activity_id': ka_act_id,
-                'title': global_ka_url,
-                'response': ka_response,
-                'response_type': ka_response_type
+                'imgID': KA_imgID,
+                'response_type': ka_response_type,
+                'answer': ka_response,
             },
-            success: function(response) {
-                console.log(response);
+            success: function ( response ) {
+                //showKAConfirmMsg("Your response was saved");
+                //console.log(response)
+                //enterLogIntoDatabase('khan academy after inserting into db, img id'+imgID, 'answer'+activity_id , answer_text, current_pagenumber)
             }
-        });
 
-        enterLogIntoDatabase('KA response input', 'Button Click', global_badge_selected, global_current_pagenumber);
+        } );
 
-    });
+        //event for failure message close button
+        $( ".upload-failure-msg-closebtn" ).on( 'click', function () {
+            var div=this.parentElement;
+            div.style.opacity="0";
+            setTimeout( function () {div.style.display="none";}, 300 );
+
+        } );
+
+
+
+
+
+
+        enterLogIntoDatabase( 'KA Bagde Copy Button Click', 'Button Click', global_badge_selected, global_current_pagenumber );
+
+
+    } );
 }
+
+
+
+
