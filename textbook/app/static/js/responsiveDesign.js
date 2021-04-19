78e9eaf064b43aeee3d9bdf1b2b6fa07ffc7caff
Xiaoxin_He <xih85@pitt.edu>
2021-04-19 01:06:34 -0400

    - [x] the first time the initial page loads, the left and right button is far from the middle line - modify the initial location of these button to match the location after resizing

--- textbook/app/static/js/responsiveDesign.js
+++ textbook/app/static/js/responsiveDesign.js
@@ -1,11 +1,25 @@
 // // add responsive design for the next and previous page button
-// $(window).resize(function() {
-//     var width = $('#textbook-content').width();
-//     // console.log("current width", width);
 
-//     $('#page-controls #page-control-previous').css("left", `${ width-100 }px`);
-//     //add 45px */
+$(document).ready(function() {
+    var currentWidth = $('#textbook-content').width();
+    init_page_button(currentWidth);
 
-//     $('#page-controls #page-control-next').css("left", `${ width-100+45 }px`);
+});
 
-// });
+const init_page_button = function(currentWidth) {
+    $('#page-controls #page-control-previous').css("left", `${ currentWidth-100 }px`);
+
+    $('#page-controls #page-control-next').css("left", `${ currentWidth-100+45 }px`);
+}
+
+$(window).resize(function() {
+
+    var width = $('#textbook-content').width();
+
+    // console.log("current width", width);
+    $('#page-controls #page-control-previous').css("left", `${ width-100 }px`);
+    //add 45px */
+
+    $('#page-controls #page-control-next').css("left", `${ width-100+45 }px`);
+
+});
